ff52ed29dedb842089e863a2e3f12b3d85b61bf5
GoldenRope <sangvd.unity@gmail.com>
2025-02-11 21:34:12 +0700

    done spinning

--- lucky-weel/LuckyWeel.cs
+++ lucky-weel/LuckyWeel.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 
 public class LuckyWeel : MonoBehaviour
 {
-    public int ROUND_MIN = 1;
-    public int SECOND_SPINNING = 3;
     public delegate void RewardDelete(int index);
     [System.Serializable]
     public enum Direction
@@ -14,25 +12,29 @@ public class LuckyWeel : MonoBehaviour
         Up, Left, Right, Down
     }
     [SerializeField] private int _itemCount;
+    [SerializeField] private Transform _parent;
     [SerializeField] private Transform _content;
     [SerializeField] private GameObject _prefab;
     [SerializeField] private GameObject _spacing;
     [SerializeField] private Direction _begin;
     [SerializeField] private RewardDelete _rewardDelegate;
     [SerializeField] private AnimationCurve curve;
+    [SerializeField] private int ROUND_MIN = 1;
+    [SerializeField] private int SECOND_SPINNING = 3;
     public RewardDelete OnRewardReceive => _rewardDelegate;
 
     private GameObject[] _items;
     private Sprite[] _itemSprites;
-    private (float, float)[] _position;
+    private (float, float)[] _angles;
     private int i;
     private float Step => -360 / _itemCount;
+    private bool isSpinning = false;
 
     private void Start()
     {
         _items = new GameObject[_itemCount];
         _itemSprites = new Sprite[_itemCount];
-        _position = new (float, float)[_itemCount];
+        _angles = new (float, float)[_itemCount];
     }
 
     [Button]
@@ -41,22 +43,26 @@ public class LuckyWeel : MonoBehaviour
         float crrAngle = 0;
         float step = Step;
         float halfStep = step / 2;
-        switch (_begin)
-        {
-            case Direction.Down: crrAngle = 180; break;
-            case Direction.Up: crrAngle = 0; break;
-            case Direction.Left: crrAngle = -90; break;
-            case Direction.Right: crrAngle = 90; break;
-        }
         for (i = 0; i < _itemCount; i++)
         {
             var item = GenItem(_prefab, crrAngle);
             //_items[i] = item;
-            _position[i] = (crrAngle + halfStep, crrAngle - halfStep);
+            _angles[i] = (crrAngle + halfStep, crrAngle - halfStep);
             item.GetComponentInChildren<TMPro.TMP_Text>().text = i.ToString();
             var spacing = GenItem(_spacing, crrAngle - halfStep);
             crrAngle += step;
         }
+        float parentAngle = 0;
+        switch (_begin)
+        {
+            case Direction.Down: parentAngle = 180; break;
+            case Direction.Up: crrAngle = 0; break;
+            case Direction.Left: parentAngle = 90; break;
+            case Direction.Right: parentAngle = -90; break;
+        }
+        var parentRotation = _parent.rotation;
+        parentRotation.eulerAngles = parentAngle * Vector3.forward;
+        _parent.rotation = parentRotation;
 
         GameObject GenItem(GameObject prefab, float crrAngle)
         {
@@ -71,62 +77,54 @@ public class LuckyWeel : MonoBehaviour
     }
 
     [Button]
-    public void MoveToZero()
-    {
-        float angle = 0;
-        var rotation = _content.transform.rotation;
-        var crrAngle = rotation.eulerAngles.z;
-        if (crrAngle > 0)
-            angle = 360 - crrAngle;
-        else
-            angle = Mathf.Abs(crrAngle);
-        crrAngle += angle;
-        rotation.eulerAngles = crrAngle * Vector3.forward;
-        _content.transform.rotation = rotation;
-    }
-
-    [Button]
     public void StartSpinning(int index = -1)
     {
+        if (isSpinning)
+            return;
         if (index < 0 || index >= _itemCount)
         {
             index = Random.Range(0, _itemCount);
         }
         Debug.Log("Index Result: " + index);
         float angle = ROUND_MIN * 360;
-        // move to zero
-        var rotation = _content.transform.rotation;
+        //MoveToZero();
+        // move to 0
+        var rotation = _content.transform.localRotation;
         var crrAngle = rotation.eulerAngles.z;
         if (crrAngle > 0)
-            angle += 360 - crrAngle;
+            crrAngle += 360 - crrAngle;
         else
-            angle += Mathf.Abs(crrAngle);
+            crrAngle = Mathf.Abs(crrAngle);
         //end move to 0
-        float angleAddFrom = Mathf.Abs(_position[index].Item1);
-        float angleAddTo = Mathf.Abs(_position[index].Item2);
-        angle += Random.Range(angleAddFrom, angleAddTo);
-        Debug.Log("Angle to from: " + angleAddFrom);
-        Debug.Log("Angle to to: " + angleAddTo);
-        Debug.Log("Crr Angle: " + crrAngle);
-        Debug.Log("Angle to Spin: " + angle);
-        StartCoroutine(Spinning(angle));
+        angle += crrAngle;
+        var center = Mathf.Abs((_angles[index].Item1 + _angles[index].Item2) / 2);
+        angle += center + Random.Range(Mathf.CeilToInt(Step / 2), Mathf.FloorToInt(-Step / 2));
+        StartCoroutine(Spinning(angle, index));
+        //rotation.eulerAngles = angle * Vector3.forward;
+        //_content.transform.rotation = rotation;
     }
 
-    private IEnumerator Spinning(float angle)
+    private IEnumerator Spinning(float angle, int index)
     {
+        isSpinning = true;
         float secondSpining = SECOND_SPINNING;
-        float speed = angle / secondSpining * Time.deltaTime;
-        var rotation = _content.transform.rotation;
+        float speed = (angle / secondSpining) * Time.deltaTime;
+        var rotation = _content.transform.localRotation;
         float crrAngle = rotation.eulerAngles.z;
-        Debug.Log("Speed: " + speed);
         WaitForFixedUpdate wait = new WaitForFixedUpdate();
+        float value = 0;
         while (crrAngle <= angle)
         {
             rotation.eulerAngles = crrAngle * Vector3.forward;
-            _content.transform.rotation = rotation;
-            crrAngle += speed;
+            _content.transform.localRotation = rotation;
+            value = curve.Evaluate(crrAngle / angle);
+            crrAngle += speed * value;
             yield return wait;
         }
+        rotation.eulerAngles = angle * Vector3.forward;
+        _content.transform.localRotation = rotation;
         Debug.Log("End");
+        OnRewardReceive?.Invoke(index);
+        isSpinning = false;
     }
 }
